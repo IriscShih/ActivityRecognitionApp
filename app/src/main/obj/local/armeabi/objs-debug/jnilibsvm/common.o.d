91ec398f33c27a1ee97ff0cf517e55bfa83ef686
Iris Shih <iris.cshih@gmail.com>
2015-12-02 10:48:24 +0100

    first commit

---
+++ app/src/main/obj/local/armeabi/objs-debug/jnilibsvm/common.o.d
@@ -0,0 +1,80 @@
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/obj/local/armeabi/objs-debug/jnilibsvm/common.o: \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/common.cpp \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/common.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream
+
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/common.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream:
---
+++ app/src/main/obj/local/armeabi/objs-debug/jnilibsvm/jnilibsvm.o.d
@@ -0,0 +1,137 @@
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/obj/local/armeabi/objs-debug/jnilibsvm/jnilibsvm.o: \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/jnilibsvm.cpp \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/./libsvm/svm-train.h \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/./libsvm/svm-predict.h \
+ /Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/common.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+ /Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/./libsvm/svm-train.h:
+
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/./libsvm/svm-predict.h:
+
+/Users/pearl790131/Desktop/android-play-location/Test/app/src/main/jni/common.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+/Users/pearl790131/Desktop/android-play-location/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream:
